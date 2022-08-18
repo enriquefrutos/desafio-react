06bf3209e8bbe1833a05adfd33abb627a1c16346
henry <enriqueafrutos@gmail.com>
2022-08-18 19:41:21 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav>
+    <ul id="menu">
+        <li><a href="index.html">INICIO</a></li>
+        <li><a href="PELICULAS.html">PELICULAS</a></li>
+        <li><a href="TRAILERS.HTML">TRAILERS</a></li>
+        <li><a href="https://www.ticketek.com.ar/">ENTRADAS</a></li>
+        <li><a href="quienesomos.html">QUIENES SOMOS</a></li>
+        <li><a href="#contacto">CONTACTO</a></li>
+        <li>TIENDA CINE</li>
+    </ul>
+</nav>
   );
 }
 
