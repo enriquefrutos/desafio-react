3b08c614d5444cb8e3dd56fe2d30f57ab66a8dbd
henry <enriqueafrutos@gmail.com>
2022-08-19 18:06:46 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-//import logo from './logo.svg';
+import NavBar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <nav>
-    <ul id="menu">
-        <li><a href="index.html">INICIO</a></li>
-        <li><a href="PELICULAS.html">PELICULAS</a></li>
-        <li><a href="TRAILERS.HTML">TRAILERS</a></li>
-        <li><a href="https://www.ticketek.com.ar/">ENTRADAS</a></li>
-        <li><a href="quienesomos.html">QUIENES SOMOS</a></li>
-        <li><a href="#contacto">CONTACTO</a></li>
-        <li>TIENDA CINE</li>
-    </ul>
+      <NavBar />
+
 </nav>
   );
 }
