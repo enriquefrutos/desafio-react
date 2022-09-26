77f06c738099b82b47ee07226bd6e805089e39b7
henry <enriqueafrutos@gmail.com>
2022-09-26 18:34:05 -0300

    c

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,51 @@
+import {collection, serverTimestamp,doc, setDoc, updateDoc, increment} from 'firebase/firestore'
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
-
+import {db} from '../utils/firebaseconfig';
 
 
 const Cart = () => {
     
     const test = useContext(CartContext);
+
+    const createorder = () => {
+        let itemsDb = test.cartList.map(item =>({
+            id: item.id,
+            title: item.name,
+            price: item.price,
+            qty: item.quantity
+        }))
+        let order = {
+            buyer: {
+                name: 'Enrique Frutos',
+                email:'enriqueafrutos@gmail.com',
+                phone:'1161564753'
+            },
+            date: serverTimestamp(),
+            items: itemsDb,
+            total: test.totalcomp()
+        }
+       
+      
+    const createorderinfirestore = async () => {
+        const newOrderRef = doc(collection(db,"orders"));
+        await setDoc(newOrderRef,order);
+        return newOrderRef;
+    }
+    createorderinfirestore()
+    .then(result => {
+        alert("Orden creada" + result.id)
+        test.cartList.forEach(async(item) => {
+        const itemRef = doc(db, "products", item.id);
+        await updateDoc(itemRef, {
+        stock: increment(-item.quantity)
+        });
+    })
+    test.clear()
+})
+    }
+
     
-
-
     return (
         <>
          <div class="containerDetail2">      
@@ -33,7 +70,8 @@ const Cart = () => {
                 }</div>
 
                 <div>
-                    TOTAL COMPRA: {test.totalcomp()}
+                    TOTAL COMPRA: {test.totalcomp()}<br></br>
+                    <button onClick={createorder}>FINALIZAR COMPRA</button>
                 </div>
 
 
